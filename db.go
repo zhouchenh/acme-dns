7fa5dd017556b159f1fae9fa44f1000b1407c972
zhouchenh <37189091+zhouchenh@users.noreply.github.com>
2025-06-24 16:39:28 +0800

    fix a bug where admin table not being created when missing

--- db.go
+++ db.go
@@ -29,7 +29,7 @@ var acmeTable = `
 var adminTable = `
 	CREATE TABLE IF NOT EXISTS admins(
         Username TEXT UNIQUE NOT NULL PRIMARY KEY,
-        Password TEXT NOT NULL,
+        Password TEXT NOT NULL
     );`
 
 var userTable = `
